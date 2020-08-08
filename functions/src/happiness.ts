2346136c0f4f9cf167aab5a4d0bfe29398f13aae
Chase Manning <me@chasemanning.co.nz>
2020-08-08 20:15:13 +1200

    Create Happienss Create Function

--- functions/src/happiness.ts
+++ functions/src/happiness.ts
@@ -1,10 +1,14 @@
 import * as functions from "firebase-functions";
 
-// const admin = require("firebase-admin");
-// admin.initializeApp();
-// const db = admin.firestore();
+import { Happiness } from "../../shared/models/happiness";
 
-export const create = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+export const create = functions.https.onRequest(async (request, response) => {
+  const docRef = db.collection("happiness").doc("qweqwe");
+  const happiness = new Happiness(6);
+  const res = await docRef.set(happiness);
+  response.send(res);
 });
