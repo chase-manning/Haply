74bab73491b9150f718b59e723991b6d04634cc5
Chase Manning <me@chasemanning.co.nz>
2020-08-09 16:46:09 +1200

    Add Typing Back for Hapiness Function

--- functions/src/happiness.ts
+++ functions/src/happiness.ts
@@ -1,25 +1,21 @@
 import * as functions from "firebase-functions";
 
+import { Happiness } from "../../shared/models/happiness";
+
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
 export const create = functions.https.onRequest(async (request, response) => {
   const col = db.collection("happiness");
-  const happiness = {
-    date: new Date(),
-    value: 8,
-  };
+  const happiness = new Happiness(6);
   const res = await col.set(happiness);
   response.send(res);
 });
 
 export const update = functions.https.onRequest(async (request, response) => {
   const docRef = db.collection("happiness").doc("qweqwe");
-  const happiness = {
-    date: new Date(),
-    value: 8,
-  };
+  const happiness = new Happiness(6);
   const res = await docRef.set(happiness);
   response.send(res);
 });
