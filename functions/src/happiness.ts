92e2e18564bb79e9f01ff24e7237b66afec3f44c
Chase Manning <me@chasemanning.co.nz>
2020-08-09 16:36:58 +1200

    Remove Type Import for Functions

--- functions/src/happiness.ts
+++ functions/src/happiness.ts
@@ -1,21 +1,25 @@
 import * as functions from "firebase-functions";
 
-import { Happiness } from "../../shared/models/happiness";
-
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
 export const create = functions.https.onRequest(async (request, response) => {
   const col = db.collection("happiness");
-  const happiness = new Happiness(6);
+  const happiness = {
+    date: new Date(),
+    value: 8,
+  };
   const res = await col.set(happiness);
   response.send(res);
 });
 
 export const update = functions.https.onRequest(async (request, response) => {
   const docRef = db.collection("happiness").doc("qweqwe");
-  const happiness = new Happiness(6);
+  const happiness = {
+    date: new Date(),
+    value: 8,
+  };
   const res = await docRef.set(happiness);
   response.send(res);
 });
