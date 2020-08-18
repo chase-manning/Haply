17d23cb8e8d0f4d472cbbbb093a70c04d322a805
Chase Manning <me@chasemanning.co.nz>
2020-08-18 21:08:30 +1200

    Create Tab One

---
+++ apps/app-mobile/src/components/tabs/TabOne/TabOne.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './TabOne.module.css';
+
+const TabOne: React.FC = () => (
+  <div className={styles.TabOne} data-testid="TabOne">
+    TabOne Component
+  </div>
+);
+
+export default TabOne;
