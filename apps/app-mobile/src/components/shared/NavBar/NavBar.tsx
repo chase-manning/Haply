1155b88abbc45a07f55c2723c50c23d7ca850a7f
Chase Manning <me@chasemanning.co.nz>
2020-08-19 18:54:29 +1200

    Change Nav Bar to Implement Nav Items Directly

--- apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
+++ apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import styles from "./NavBar.module.css";
-import NavItem from "../NavItem/NavItem";
+import AccessAlarm from "@material-ui/icons/AccessAlarm";
 
 const NavBar: React.FC = () => (
   <div className={styles.NavBar} data-testid="NavBar">
-    <NavItem></NavItem>
-    <NavItem></NavItem>
-    <NavItem></NavItem>
-    <NavItem></NavItem>
+    <a href="/" className={styles.NavItem}>
+      <AccessAlarm />
+      <span>Meow</span>
+    </a>
+    <a href="/users" className={styles.NavItem}>
+      <AccessAlarm />
+      <span>Meow</span>
+    </a>
+    <a href="/" className={styles.NavItem}>
+      <AccessAlarm />
+      <span>Meow</span>
+    </a>
+    <a href="/" className={styles.NavItem}>
+      <AccessAlarm />
+      <span>Meow</span>
+    </a>
   </div>
 );
 
