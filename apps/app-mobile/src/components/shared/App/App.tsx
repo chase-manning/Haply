ae625e9a9eafc61bf9f944fdabb2e351ff0c2413
Chase Manning <me@chasemanning.co.nz>
2020-08-21 21:12:37 +1200

    Update App to Direct to new Tabs

--- apps/app-mobile/src/components/shared/App/App.tsx
+++ apps/app-mobile/src/components/shared/App/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import TabOne from "../../tabs/TabOne/TabOne";
-import TabTwo from "../../tabs/TabTwo/TabTwo";
+import Profile from "../../tabs/Profile/Profile";
+import Entries from "../../tabs/Entries/Entries";
+import Capture from "../../tabs/Capture/Capture";
+import Stats from "../../tabs/Stats/Stats";
+import Settings from "../../tabs/Settings/Settings";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={TabOne} />
-        <Route path="/users" component={TabTwo} />
+        <Route exact path="/" component={Profile} />
+        <Route path="/entries" component={Entries} />
+        <Route path="/capture" component={Capture} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/settings" component={Settings} />
       </Router>
     </div>
   );
