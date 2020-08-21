80cb9dbe6bd657ed5fbfe75dbebd299001fe6b59
Chase Manning <me@chasemanning.co.nz>
2020-08-21 21:11:51 +1200

    Create Settings Tab Component

---
+++ apps/app-mobile/src/components/tabs/Settings/Settings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Settings.module.css';
+
+const Settings: React.FC = () => (
+  <div className={styles.Settings} data-testid="Settings">
+    Settings Component
+  </div>
+);
+
+export default Settings;
