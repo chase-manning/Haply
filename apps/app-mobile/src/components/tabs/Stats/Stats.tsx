61757ff7addd6b61917d1bc55004a6378f0b9ad3
Chase Manning <me@chasemanning.co.nz>
2020-08-21 21:12:04 +1200

    Create Stats Tab Component

---
+++ apps/app-mobile/src/components/tabs/Stats/Stats.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Stats.module.css';
+
+const Stats: React.FC = () => (
+  <div className={styles.Stats} data-testid="Stats">
+    Stats Component
+  </div>
+);
+
+export default Stats;
