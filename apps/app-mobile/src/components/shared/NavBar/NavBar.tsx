03ae0a9b55d78c4a130e4e46803ff1a4e18a5083
Chase Manning <me@chasemanning.co.nz>
2020-08-21 21:02:21 +1200

    Create Basic Tab Bar Labels

--- apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
+++ apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import styles from "./NavBar.module.css";
 import AccessAlarm from "@material-ui/icons/AccessAlarm";
+import AddBox from "@material-ui/icons/AddBox";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import AccountBox from "@material-ui/icons/AccountBox";
+import InsertChart from "@material-ui/icons/InsertChart";
+import Settings from "@material-ui/icons/Settings";
 
 const NavBar: React.FC = () => (
   <div className={styles.NavBar} data-testid="NavBar">
     <a href="/" className={styles.NavItem}>
-      <AccessAlarm />
-      <span>Meow</span>
+      <AccountBox />
+      <span>Profile</span>
+    </a>
+    <a href="/" className={styles.NavItem}>
+      <LibraryBooks />
+      <span>Entries</span>
     </a>
     <a href="/users" className={styles.NavItem}>
-      <AccessAlarm />
-      <span>Meow</span>
+      <AddBox />
+      <span>Capture</span>
     </a>
     <a href="/" className={styles.NavItem}>
-      <AccessAlarm />
-      <span>Meow</span>
+      <InsertChart />
+      <span>Stats</span>
     </a>
     <a href="/" className={styles.NavItem}>
-      <AccessAlarm />
-      <span>Meow</span>
+      <Settings />
+      <span>Settings</span>
     </a>
   </div>
 );
