888af6ef58c3f27501fae6bf6db0ff18511705c6
Chase Manning <me@chasemanning.co.nz>
2020-08-21 20:43:08 +1200

    Add a button to test api call

--- apps/app-mobile/src/components/tabs/TabOne/TabOne.tsx
+++ apps/app-mobile/src/components/tabs/TabOne/TabOne.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import styles from './TabOne.module.css';
+import React from "react";
+import styles from "./TabOne.module.css";
 
 const TabOne: React.FC = () => (
   <div className={styles.TabOne} data-testid="TabOne">
     TabOne Component
+    <button onClick={meow}>Meow</button>
   </div>
 );
+
+function meow(): void {
+  console.log("werwerA");
+  fetch(
+    "https://us-central1-happiness-software.cloudfunctions.net/happiness-create"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 
 export default TabOne;
