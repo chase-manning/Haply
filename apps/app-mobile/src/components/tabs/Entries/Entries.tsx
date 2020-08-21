1b0aa5df1228bb7a1722dbbd9b75b99ffa7649ad
Chase Manning <me@chasemanning.co.nz>
2020-08-21 21:11:31 +1200

    Create Entries Tab Component

---
+++ apps/app-mobile/src/components/tabs/Entries/Entries.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Entries.module.css';
+
+const Entries: React.FC = () => (
+  <div className={styles.Entries} data-testid="Entries">
+    Entries Component
+  </div>
+);
+
+export default Entries;
