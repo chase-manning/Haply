3047a1153323384353e37e35c457bb89ff9e7598
Chase Manning <me@chasemanning.co.nz>
2020-08-22 21:25:49 +1200

    Change Settings to Extend React Component

--- apps/app-mobile/src/components/tabs/Settings/Settings.tsx
+++ apps/app-mobile/src/components/tabs/Settings/Settings.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import styles from './Settings.module.css';
+import React, { Component } from "react";
+import styles from "./Settings.module.css";
 
-const Settings: React.FC = () => (
-  <div className={styles.Settings} data-testid="Settings">
-    Settings Component
-  </div>
-);
-
-export default Settings;
+export default class Settings extends Component {
+  render() {
+    return (
+      <div className={styles.Settings} data-testid="Settings">
+        Settings Component
+      </div>
+    );
+  }
+}
