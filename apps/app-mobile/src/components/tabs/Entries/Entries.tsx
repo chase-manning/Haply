564f799243248d08219fa23a6af976550e89ff5e
Chase Manning <me@chasemanning.co.nz>
2020-08-22 21:25:37 +1200

    Change Entries to Extend React Component

--- apps/app-mobile/src/components/tabs/Entries/Entries.tsx
+++ apps/app-mobile/src/components/tabs/Entries/Entries.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import styles from './Entries.module.css';
+import React, { Component } from "react";
+import styles from "./Entries.module.css";
 
-const Entries: React.FC = () => (
-  <div className={styles.Entries} data-testid="Entries">
-    Entries Component
-  </div>
-);
-
-export default Entries;
+export default class Entries extends Component {
+  render() {
+    return (
+      <div className={styles.Entries} data-testid="Entries">
+        Entries Component
+      </div>
+    );
+  }
+}
