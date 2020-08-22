9bac346a6071ea1db47413943f97df4402510f9b
Chase Manning <me@chasemanning.co.nz>
2020-08-22 12:29:25 +1200

    Convert Admin to Import

--- functions/src/mood.ts
+++ functions/src/mood.ts
@@ -1,8 +1,8 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 import { Mood } from "../../shared/models/mood";
 
-const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
