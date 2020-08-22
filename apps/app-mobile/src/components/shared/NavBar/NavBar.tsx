c25e993312765704eceae94b42590b94f511f917
Chase Manning <me@chasemanning.co.nz>
2020-08-22 21:25:28 +1200

    Change NavBar to Extend React Component

--- apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
+++ apps/app-mobile/src/components/shared/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./NavBar.module.css";
 import AddBox from "@material-ui/icons/AddBox";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
@@ -6,29 +6,31 @@ import AccountBox from "@material-ui/icons/AccountBox";
 import InsertChart from "@material-ui/icons/InsertChart";
 import Settings from "@material-ui/icons/Settings";
 
-const NavBar: React.FC = () => (
-  <div className={styles.NavBar} data-testid="NavBar">
-    <a href="/" className={styles.NavItem}>
-      <AccountBox />
-      <span>Profile</span>
-    </a>
-    <a href="/entries" className={styles.NavItem}>
-      <LibraryBooks />
-      <span>Entries</span>
-    </a>
-    <a href="/capture" className={styles.NavItem}>
-      <AddBox />
-      <span>Capture</span>
-    </a>
-    <a href="/stats" className={styles.NavItem}>
-      <InsertChart />
-      <span>Stats</span>
-    </a>
-    <a href="/settings" className={styles.NavItem}>
-      <Settings />
-      <span>Settings</span>
-    </a>
-  </div>
-);
-
-export default NavBar;
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className={styles.NavBar} data-testid="NavBar">
+        <a href="/" className={styles.NavItem}>
+          <AccountBox />
+          <span>Profile</span>
+        </a>
+        <a href="/entries" className={styles.NavItem}>
+          <LibraryBooks />
+          <span>Entries</span>
+        </a>
+        <a href="/capture" className={styles.NavItem}>
+          <AddBox />
+          <span>Capture</span>
+        </a>
+        <a href="/stats" className={styles.NavItem}>
+          <InsertChart />
+          <span>Stats</span>
+        </a>
+        <a href="/settings" className={styles.NavItem}>
+          <Settings />
+          <span>Settings</span>
+        </a>
+      </div>
+    );
+  }
+}
