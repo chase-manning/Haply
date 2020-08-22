dc852e8dc65b89d26fad42906caf5a30f7f8bcf6
Chase Manning <me@chasemanning.co.nz>
2020-08-22 12:59:32 +1200

    Create Mood CRUD

--- functions/src/mood.ts
+++ functions/src/mood.ts
@@ -1,21 +1,116 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-import { Mood } from "../../shared/models/mood";
+//import { Mood } from "../../shared/models/mood";
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
-export const create = functions.https.onRequest(async (request, response) => {
-  const col = db.collection("mood");
-  const happiness = new Mood(6);
-  const res = await col.add(happiness.json);
-  response.send(res);
+const app = express();
+const main = express();
+
+main.use("/api", app);
+main.use(bodyParser.json());
+
+export const webApi = functions.https.onRequest(main);
+
+app.post("/moods", async (request, response) => {
+  try {
+    const { value, date } = request.body;
+    const data = {
+      value,
+      date,
+    };
+
+    const moodRef = await db.collection("moods").add(data);
+    const mood = await moodRef.get();
+
+    response.json({
+      id: moodRef.id,
+      data: mood.data(),
+    });
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
-export const update = functions.https.onRequest(async (request, response) => {
-  const col = db.collection("mood").doc("qweqwe");
-  const happiness = new Mood(6);
-  const res = await col.set(happiness.json);
-  response.send(res);
+app.get("/moods/:id", async (request, response) => {
+  try {
+    const moodId = request.params.id;
+
+    if (!moodId) throw new Error("Mood ID is required");
+
+    const mood = await db.collection("fights").doc(moodId).get();
+
+    if (!mood.exists) {
+      throw new Error("Mood doesnt exist.");
+    }
+
+    response.json({
+      id: mood.id,
+      data: mood.data(),
+    });
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/moods", async (request, response) => {
+  try {
+    const moodQuerySnapshot = await db.collection("moods").get();
+    const moods: any[] = [];
+    moodQuerySnapshot.forEach((doc) => {
+      moods.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+
+    response.json(moods);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.put("/moods/:id", async (request, response) => {
+  try {
+    const moodId = request.params.id;
+    const { value, date } = request.body;
+    const data = {
+      value,
+      date,
+    };
+
+    if (!moodId) throw new Error("id is blank");
+
+    const moodRef = await db
+      .collection("moods")
+      .doc(moodId)
+      .set(data, { merge: true });
+
+    response.json({
+      id: moodId,
+      data,
+    });
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.delete("/moods/:id", async (request, response) => {
+  try {
+    const moodId = request.params.id;
+
+    if (!moodId) throw new Error("id is blank");
+
+    await db.collection("moods").doc(moodId).delete();
+
+    response.json({
+      id: moodId,
+    });
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
