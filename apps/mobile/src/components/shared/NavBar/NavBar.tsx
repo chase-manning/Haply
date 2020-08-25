b84ebb6507d5165cc90c08131aad27d7e049c858
Chase Manning <me@chasemanning.co.nz>
2020-08-25 17:13:54 +1200

    Convert Nav Bar to Styled Components

--- apps/mobile/src/components/shared/NavBar/NavBar.tsx
+++ apps/mobile/src/components/shared/NavBar/NavBar.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import styles from "./NavBar.module.css";
 import AddBox from "@material-ui/icons/AddBox";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import AccountBox from "@material-ui/icons/AccountBox";
@@ -7,31 +6,56 @@ import InsertChart from "@material-ui/icons/InsertChart";
 import Settings from "@material-ui/icons/Settings";
 import styled from "styled-components";
 
+const StyledNavBar = styled.div`
+  height: 10%;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: white;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const NavItem = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  margin: auto;
+  width: 100%;
+  text-decoration: none;
+  color: black;
+`;
+
 export default class NavBar extends Component {
   render() {
     return (
-      <div className={styles.NavBar} data-testid="NavBar">
-        <a href="/" className={styles.NavItem}>
+      <StyledNavBar data-testid="NavBar">
+        <NavItem href="/">
           <AccountBox />
           <span>Profile</span>
-        </a>
-        <a href="/entries" className={styles.NavItem}>
+        </NavItem>
+        <NavItem href="/entries">
           <LibraryBooks />
           <span>Entries</span>
-        </a>
-        <a href="/capture" className={styles.NavItem}>
+        </NavItem>
+        <NavItem href="/capture">
           <AddBox />
           <span>Capture</span>
-        </a>
-        <a href="/stats" className={styles.NavItem}>
+        </NavItem>
+        <NavItem href="/stats">
           <InsertChart />
           <span>Stats</span>
-        </a>
-        <a href="/settings" className={styles.NavItem}>
+        </NavItem>
+        <NavItem href="/settings">
           <Settings />
           <span>Settings</span>
-        </a>
-      </div>
+        </NavItem>
+      </StyledNavBar>
     );
   }
 }
