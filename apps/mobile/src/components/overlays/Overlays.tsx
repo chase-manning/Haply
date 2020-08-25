4d9f092c12459b419e0a7440427ff3b287be13f9
Chase Manning <me@chasemanning.co.nz>
2020-08-26 00:10:39 +1200

    Create Overlays Component

---
+++ apps/mobile/src/components/overlays/Overlays.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledOverlays = styled.div``;
+
+export default class Overlays extends Component {
+  render() {
+    return <StyledOverlays data-testid="Overlays"></StyledOverlays>;
+  }
+}
