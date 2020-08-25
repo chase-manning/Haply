213169e545ac4b351e4a6d2629e1b37c27e955b7
Chase Manning <me@chasemanning.co.nz>
2020-08-25 17:22:23 +1200

    Convert Settings to Styled Components

--- apps/mobile/src/components/tabs/Settings/Settings.tsx
+++ apps/mobile/src/components/tabs/Settings/Settings.tsx
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import styles from "./Settings.module.css";
 import styled from "styled-components";
 
 export default class Settings extends Component {
   render() {
-    return (
-      <div className={styles.Settings} data-testid="Settings">
-        Settings Component
-      </div>
-    );
+    return <div data-testid="Settings">Settings Component</div>;
   }
 }
