c7beef37388dcb4bc598cfca185b741127ddfe0b
Chase Manning <me@chasemanning.co.nz>
2020-08-25 17:22:11 +1200

    Convert Entried to Styled Components

--- apps/mobile/src/components/tabs/Entries/Entries.tsx
+++ apps/mobile/src/components/tabs/Entries/Entries.tsx
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import styles from "./Entries.module.css";
 import styled from "styled-components";
 
 export default class Entries extends Component {
   render() {
-    return (
-      <div className={styles.Entries} data-testid="Entries">
-        Entries Component
-      </div>
-    );
+    return <div data-testid="Entries">Entries Component</div>;
   }
 }
