0e0fcc1096b6fadd640342802483612c647a9227
Chase Manning <me@chasemanning.co.nz>
2020-08-26 10:40:46 +1200

    Format Capture as Overlay

--- apps/mobile/src/components/overlays/Capture.tsx
+++ apps/mobile/src/components/overlays/Capture.tsx
@@ -3,6 +3,11 @@ import Mood from "../../models/mood";
 import styled from "styled-components";
 
 const StyledCapture = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   height: 100%;
