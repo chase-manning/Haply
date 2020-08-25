b0222b490584f4a6844604cc45c295d241d90126
Chase Manning <me@chasemanning.co.nz>
2020-08-25 17:22:29 +1200

    Convert Stats to Styled Components

--- apps/mobile/src/components/tabs/Stats/Stats.tsx
+++ apps/mobile/src/components/tabs/Stats/Stats.tsx
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import styles from "./Stats.module.css";
 import styled from "styled-components";
 
 export default class Stats extends Component {
   render() {
-    return (
-      <div className={styles.Stats} data-testid="Stats">
-        Stats Component
-      </div>
-    );
+    return <div data-testid="Stats">Stats Component</div>;
   }
 }
