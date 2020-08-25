80939c804da0ec4992aa9457513724b97af70e79
Chase Manning <me@chasemanning.co.nz>
2020-08-25 17:22:17 +1200

    Convert Profile to Styled Components

--- apps/mobile/src/components/tabs/Profile/Profile.tsx
+++ apps/mobile/src/components/tabs/Profile/Profile.tsx
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import styles from "./Profile.module.css";
 
 export default class Profile extends Component {
   render() {
-    return (
-      <div className={styles.Profile} data-testid="Profile">
-        Profile Component
-      </div>
-    );
+    return <div data-testid="Profile">Profile Component</div>;
   }
 }
