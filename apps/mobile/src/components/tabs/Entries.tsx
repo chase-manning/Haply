d4ab4f9f052c2e0e6a8d7a0852281faeed123b5f
Chase Manning <me@chasemanning.co.nz>
2020-08-28 21:09:08 +1200

    Add Drop Shadow to Header

--- apps/mobile/src/components/tabs/Entries.tsx
+++ apps/mobile/src/components/tabs/Entries.tsx
@@ -21,6 +21,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const TabContent = styled.div`
