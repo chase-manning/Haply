7707cdcf1f6f91eb8f109af5a525b0679428dd30
Chase Manning <me@chasemanning.co.nz>
2020-08-28 20:40:58 +1200

    Get Moods And Display as Entries

--- apps/mobile/src/components/tabs/Entries.tsx
+++ apps/mobile/src/components/tabs/Entries.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Entry from "../shared/Entry";
+import MoodService from "../../services/MoodService";
+import Mood from "../../models/mood";
 
 const StyledEntries = styled.div`
   height: 100%;
@@ -27,15 +29,40 @@ const TabContent = styled.div`
   padding: 10px;
 `;
 
+class EntriesState {
+  moods: Mood[] = [];
+}
+
 export default class Entries extends Component {
+  state: EntriesState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = new EntriesState();
+  }
+
+  componentDidMount() {
+    this.getMoods();
+  }
+
   render() {
+    const entries: JSX.Element[] = [];
+    this.state.moods.forEach((mood) => {
+      entries.push(<Entry />);
+    });
+
     return (
       <StyledEntries data-testid="Entries">
         <Header>Entries</Header>
-        <TabContent>
-          <Entry></Entry>
-        </TabContent>
+        <TabContent>{entries}</TabContent>
       </StyledEntries>
     );
+  }
+
+  async getMoods(): Promise<void> {
+    const response = await MoodService.getMoods();
+    const moods: Mood[] = await response.json();
+    this.setState({ moods: moods });
+    console.log(this.state);
   }
 }
