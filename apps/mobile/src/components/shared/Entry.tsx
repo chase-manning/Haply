1cc4f9761b9e3ada8b198b54becbe9f7d6a480a4
Chase Manning <me@chasemanning.co.nz>
2020-08-28 20:21:33 +1200

    Move Entry to Seperate Component

---
+++ apps/mobile/src/components/shared/Entry.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+
+const StyledEntry = styled.div`
+  width: 100%;
+  margin: 10px 0;
+  background-color: white;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
+  padding: 15px 20px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--sub);
+`;
+
+const EntryText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const EntryHeader = styled.div`
+  color: black;
+  margin-bottom: 10px;
+`;
+
+const EntrySubHeader = styled.div`
+  color: var(--sub);
+  font-size: 12px;
+`;
+
+export default class Entry extends Component {
+  render() {
+    return (
+      <StyledEntry data-testid="Entries">
+        <EntryText>
+          <EntryHeader>Awesome!</EntryHeader>
+          <EntrySubHeader>Monday, 2:34pm</EntrySubHeader>
+        </EntryText>
+        <DeleteOutline />
+      </StyledEntry>
+    );
+  }
+}
--- apps/mobile/src/components/tabs/Entries.tsx
+++ apps/mobile/src/components/tabs/Entries.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Entry from "../shared/Entry";
 
 const StyledEntries = styled.div`
   height: 100%;
@@ -27,48 +27,13 @@ const TabContent = styled.div`
   padding: 10px;
 `;
 
-const Entry = styled.div`
-  width: 100%;
-  margin: 10px 0;
-  background-color: white;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
-  padding: 15px 20px;
-  border-radius: 5px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: var(--sub);
-`;
-
-const EntryText = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const EntryHeader = styled.div`
-  color: black;
-  margin-bottom: 10px;
-`;
-
-const EntrySubHeader = styled.div`
-  color: var(--sub);
-  font-size: 12px;
-`;
-
 export default class Entries extends Component {
   render() {
     return (
       <StyledEntries data-testid="Entries">
         <Header>Entries</Header>
         <TabContent>
-          <Entry>
-            <EntryText>
-              <EntryHeader>Awesome!</EntryHeader>
-              <EntrySubHeader>Monday, 2:34pm</EntrySubHeader>
-            </EntryText>
-            <DeleteOutline />
-          </Entry>
+          <Entry></Entry>
         </TabContent>
       </StyledEntries>
     );
