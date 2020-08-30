b6b7c0564f3702c057687563be7686a185769c85
Chase Manning <me@chasemanning.co.nz>
2020-08-30 23:00:42 +1200

    Add Styled Components to Stats Tab

--- apps/mobile/src/components/tabs/Stats.tsx
+++ apps/mobile/src/components/tabs/Stats.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledStats = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: red;
+`;
 
 export default class Stats extends Component {
   render() {
-    return <div data-testid="Stats">Stats Component</div>;
+    return <StyledStats data-testid="Stats">Stats Component</StyledStats>;
   }
 }
