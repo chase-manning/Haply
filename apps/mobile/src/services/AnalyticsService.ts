93a50f994af9bbb3b8c91c25846d2972a4621e59
Chase Manning <me@chasemanning.co.nz>
2020-08-31 14:23:14 +1200

    Create Analytics Service File

---
+++ apps/mobile/src/services/AnalyticsService.ts
@@ -0,0 +1,10 @@
+//TODO Fix the names of the insights, analaytics, stats stuff to be consistant
+
+const AnalyticsService = {
+  mew(): void {
+    let meow = "meow";
+    console.log(meow);
+  },
+};
+
+export default AnalyticsService;
