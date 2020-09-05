9e3ca965f9692f8014cd6f09f04caf4abf984a5b
Chase Manning <me@chasemanning.co.nz>
2020-09-06 11:37:07 +1200

    Create Line Styled Component

---
+++ apps/mobile/src/styles/Line.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Line = styled.div`
+  width: 100%;
+  padding: 15px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--sub);
+  border-bottom: 1px solid var(--border);
+`;
