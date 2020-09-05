c9208102b6b1dc3fab5040b22444b7167207d9ca
Chase Manning <me@chasemanning.co.nz>
2020-09-06 11:50:39 +1200

    Remove Redundant Styles

--- apps/mobile/src/components/tabs/Settings.tsx
+++ apps/mobile/src/components/tabs/Settings.tsx
@@ -10,30 +10,6 @@ const StyledSettings = styled.div`
   padding: 0 20px;
 `;
 
-const Meow = styled.div`
-  position: absolute;
-  right: 15px;
-  color: var(--sub);
-`;
-
-const Section = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
-  position: relative;
-`;
-
-const Setting = styled.div`
-  width: 100%;
-  padding: 15px;
-  display: flex;
-  justify-content: space-between;
-  background-color: white;
-  margin-bottom: 2px;
-`;
-
 const Label = styled.div`
   color: var(--sub);
   width: 50%;
