4d6cc8b5fa6aae71ed87bd051235ed23e9b30677
Chase Manning <me@chasemanning.co.nz>
2020-09-06 11:16:17 +1200

    Create Card Styled Components

---
+++ apps/mobile/src/styles/card.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  width: 100%;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  margin: 5px 0px;
+  border: solid 1px var(--border);
+`;
