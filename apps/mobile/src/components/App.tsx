e8bc1127df1b2c822649a63be34ce0278cff7fbb
Chase Manning <me@chasemanning.co.nz>
2020-09-06 11:36:26 +1200

    Remove Shadow from Header

--- apps/mobile/src/components/App.tsx
+++ apps/mobile/src/components/App.tsx
@@ -38,7 +38,6 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const TabContent = styled.div`
