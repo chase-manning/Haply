5195ef1665cf44a621e82f27e8b7ac51a0eb0720
Chase Manning <me@chasemanning.co.nz>
2020-09-08 20:26:10 +1200

    Remove Profile Content and Replace with Acievement

--- apps/mobile/src/components/tabs/Profile.tsx
+++ apps/mobile/src/components/tabs/Profile.tsx
@@ -1,76 +1,13 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import PersonOutline from "@material-ui/icons/PersonOutline";
-import WhatshotOutlined from "@material-ui/icons/WhatshotOutlined";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import MoodService from "../../services/MoodService";
 import { User } from "firebase";
-import { Card } from "../../styles/Card";
 
 const StyledProfile = styled.div`
   width: 100%;
   height: 100%;
   padding: 20px;
-`;
-
-const Photo = styled.div`
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  background-color: var(--primary-light);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  margin-bottom: 20px;
-  position: relative;
-`;
-
-const HappinessScale = styled.div`
-  position: absolute;
-  top: -13px;
-  left: -13px;
-`;
-
-const Name = styled.div`
-  color: black;
-  font-size: 20px;
-`;
-
-const Description = styled.div`
-  color: var(--sub);
-  font-size: 14px;
-  margin-top: 5px;
-`;
-
-const StatusContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 20px;
-  width: 150px;
-`;
-
-const Status = styled.div`
-  border-radius: 14px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 7px 14px;
-  font-size: 14px;
-  color: var(--sub);
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.07);
-`;
-
-const StatusNumber = styled.div`
-  color: var(--primary);
-  margin-left: 5px;
-`;
-
-const Header = styled.div`
-  font-size: 20px;
-  margin-bottom: 20px;
+  overflow: auto;
 `;
 
 const Acheivements = styled.div`
@@ -109,10 +46,7 @@ const AcheivementName = styled.div`
   text-align: center;
 `;
 
-class ProfileState {
-  averageHappiness: number = 0;
-  dailyStreak: string = "-";
-}
+class ProfileState {}
 
 type Props = {
   user: User;
@@ -146,39 +80,7 @@ export default class Profile extends Component<Props> {
 
     return (
       <StyledProfile data-testid="Profile">
-        <Card>
-          <Photo>
-            <HappinessScale>
-              <CircularProgress
-                value={this.state.averageHappiness}
-                variant="static"
-                size="176px"
-                thickness={1.5}
-                style={{
-                  color: "var(--primary)",
-                  strokeLinecap: "round",
-                }}
-              />
-            </HappinessScale>
-            <PersonOutline style={{ transform: "scale(4)" }} />
-          </Photo>
-          {false && <Name>Chase Manning</Name>}
-          {false && <Description>Apprentice</Description>}
-          {false && (
-            <StatusContainer>
-              <Status>
-                <WhatshotOutlined fontSize="inherit" />
-                <StatusNumber>33</StatusNumber>
-              </Status>
-            </StatusContainer>
-          )}
-        </Card>
-        {false && (
-          <Card>
-            <Header>Acheivements</Header>
-            <Acheivements>{acheivements}</Acheivements>
-          </Card>
-        )}
+        <Acheivements>{acheivements}</Acheivements>
       </StyledProfile>
     );
   }
