a6855da403c05b51f26559ac19cde39b8241c623
Chase Manning <me@chasemanning.co.nz>
2020-09-09 14:38:54 +1200

    Remove Achievement Gradient

--- apps/mobile/src/components/shared/Achievement.tsx
+++ apps/mobile/src/components/shared/Achievement.tsx
@@ -70,11 +70,6 @@ const Overlay = styled.div`
   border-radius: 50%;
   border-top: solid 2px rgba(255, 255, 255, 0.5);
   border-left: solid 2px rgba(255, 255, 255, 0.5);
-  background: linear-gradient(
-    125deg,
-    rgba(255, 255, 255, 0.3),
-    rgba(0, 0, 0, 0.2)
-  );
 `;
 
 type Props = {
