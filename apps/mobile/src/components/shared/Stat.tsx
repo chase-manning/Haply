c130c3345a2f547b97448b25002db806e36d29ec
Chase Manning <me@chasemanning.co.nz>
2020-09-11 18:45:55 +1200

    Add Locked Stat Feature

--- apps/mobile/src/components/shared/Stat.tsx
+++ apps/mobile/src/components/shared/Stat.tsx
@@ -4,13 +4,49 @@ import { Card } from "../../styles/Card";
 import { StatModel, StatType } from "../../models/StatModel";
 import StatBar from "./StatBar";
 import StatChart from "./StatChart";
+import noData from "../../assets/svgs/undraw_no_data_qbuo.svg";
 
 const Header = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
-  font-size: 20px;
+  font-size: 18px;
+`;
+
+const LockedStat = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Svg = styled.img`
+  width: 40%;
+`;
+
+const Description = styled.div`
+  font-size: 12px;
+  margin-top: 15px;
+  color: var(--sub);
+  width: 100%;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const ProgressBar = styled.div`
+  width: 70%;
+  margin-top: 10px;
+  height: 6px;
+  border-radius: 3px;
+  background-color: var(--primary-light);
+`;
+
+const Complete = styled.div`
+  height: 6px;
+  border-radius: 3px;
+  background-color: var(--primary);
+  width: 50%;
 `;
 
 type Props = {
@@ -22,10 +58,19 @@ export default class Stat extends Component<Props> {
     return (
       <Card data-testid="Stat">
         <Header>{this.props.stat.title}</Header>
-        {this.props.stat.type === StatType.Bar && (
+        {this.props.stat.locked && (
+          <LockedStat>
+            <Svg src={noData} />
+            <Description>{this.props.stat.lockedMessage}</Description>
+            <ProgressBar>
+              <Complete />
+            </ProgressBar>
+          </LockedStat>
+        )}
+        {!this.props.stat.locked && this.props.stat.type === StatType.Bar && (
           <StatBar dataPoints={this.props.stat.dataPoints}></StatBar>
         )}
-        {this.props.stat.type === StatType.Chart && (
+        {!this.props.stat.locked && this.props.stat.type === StatType.Chart && (
           <StatChart dataPoints={this.props.stat.dataPoints}></StatChart>
         )}
       </Card>
