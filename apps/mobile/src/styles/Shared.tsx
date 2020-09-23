a71dff48fe2c9d61eb80d4f98a1403bec29df472
Chase Manning <me@chasemanning.co.nz>
2020-09-24 08:37:18 +1200

    Create Shared Styles with Selected Tag

---
+++ apps/mobile/src/styles/Shared.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SelectedTag = styled.button`
+  padding: 5px 8px;
+  border-radius: 12px;
+  background-color: var(--primary-light);
+  font-size: 12px;
+  margin: 0 5px 5px 0;
+  color: var(--primary);
+  display: inline-block;
+  text-overflow: ellipsis;
+  border: none;
+  outline: none;
+`;
