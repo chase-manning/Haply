4556060829ca74a5332dec4fda10d67269ecc430
Chase Manning <me@chasemanning.co.nz>
2020-09-24 22:09:53 +1200

    Remove Global Styles from Index

--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -2,35 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  :root {
-    --main: black;
-    --sub: #9399A9;
-    --sub-light: rgba(147,154,169, 0.1);
-    --primary: #4071FE; 
-    --primary-light: rgba(64,114,253,0.1);
-    --primary-shadow: drop-shadow( 0px 5px 10px rgba(64,114,253, 0.5));
-    --highlight: #FF6584;
-    --bg: #F9FAFC;
-    --blue: #5A8BFF;
-    --yellow: #FFCF66;
-    --red: #FF6C8E;
-    --border: rgba(0,0,0,0.1);
-  }
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    font-family: "Roboto", sans-serif;
-  }
-`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App></App>
   </React.StrictMode>,
   document.getElementById("root")
