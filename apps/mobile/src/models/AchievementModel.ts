6c41a0a9dab696791abcc7d5b4ae58e06abfeba7
Chase Manning <me@chasemanning.co.nz>
2020-09-25 20:43:00 +1200

    Add Unlocks to Achievement Model

--- apps/mobile/src/models/AchievementModel.ts
+++ apps/mobile/src/models/AchievementModel.ts
@@ -4,18 +4,21 @@ export default class AchievementModel {
   title: string;
   description: string;
   colorPrimary: string;
+  unlocks: string[];
 
   constructor(
     svg: string,
     percentComplete: number,
     title: string,
     description: string,
-    colorPrimary: string
+    colorPrimary: string,
+    unlocks: string[] = []
   ) {
     this.svg = svg;
     this.percentComplete = percentComplete;
     this.title = title;
     this.description = description;
     this.colorPrimary = colorPrimary;
+    this.unlocks = unlocks;
   }
 }
