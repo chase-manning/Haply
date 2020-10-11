b83a1437a082cf23f7dff6996d9f2891c307735c
Chase Manning <me@chasemanning.co.nz>
2020-10-11 17:11:43 +1300

    Add User to Store

--- apps/mobile/src/state/store.ts
+++ apps/mobile/src/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import navigationReducer from "./navigationSlice";
+import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     navigation: navigationReducer,
+    user: userReducer,
   },
 });
 
