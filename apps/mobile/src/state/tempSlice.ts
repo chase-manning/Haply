fa0fb6e2a3911a5341d1a421eaa6a62e37acd9dd
Chase Manning <me@chasemanning.co.nz>
2020-10-11 20:38:10 +1300

    Create Temp Slice

---
+++ apps/mobile/src/state/tempSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Mode } from "../models/state";
+import { AppThunk, RootState } from "./store";
+
+/* TYPES */
+interface TempState {
+  tagOptions: string[];
+  colorPrimary: string;
+  mode: Mode;
+}
+
+const initialState: TempState = {
+  tagOptions: [
+    "With Friends",
+    "With Family",
+    "Tired",
+    "Well Rested",
+    "In Pain",
+    "Sick",
+    "Working",
+    "At Home",
+    "On Holiday",
+    "Eating Well",
+    "Eating Poorly",
+    "Exercising",
+  ],
+  colorPrimary: "#4071fe",
+  mode: Mode.Default,
+};
+
+/* SLICE */
+export const tempSlice = createSlice({
+  name: "temp",
+  initialState,
+  reducers: {
+    setTagOptions: (state, action: PayloadAction<string[]>) => {
+      state.tagOptions = action.payload;
+    },
+    setColorPrimary: (state, action: PayloadAction<string>) => {
+      state.colorPrimary = action.payload;
+    },
+    setMode: (state, action: PayloadAction<Mode>) => {
+      state.mode = action.payload;
+    },
+  },
+});
+
+export const { setTagOptions, setColorPrimary, setMode } = tempSlice.actions;
+
+/* SELECTS */
+export const selectTagOptions = (state: RootState) => state.temp.tagOptions;
+export const selectColorPrimary = (state: RootState) => state.temp.colorPrimary;
+export const selectMode = (state: RootState) => state.temp.mode;
+
+export default tempSlice.reducer;
