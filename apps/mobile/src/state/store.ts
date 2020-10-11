ab3c473885c94357a07156b21c84d1f82d673c91
Chase Manning <me@chasemanning.co.nz>
2020-10-11 20:18:52 +1300

    Add Settings Slice to Store

--- apps/mobile/src/state/store.ts
+++ apps/mobile/src/state/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import navigationReducer from "./navigationSlice";
 import userReducer from "./userSlice";
+import settingsReducer from "./settingsSlice";
 
 export const store = configureStore({
   reducer: {
     navigation: navigationReducer,
     user: userReducer,
+    settings: settingsReducer,
   },
 });
 
