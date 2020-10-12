f4b1be56ebd659c05b112dfb13d3c7813eab08a4
Chase Manning <me@chasemanning.co.nz>
2020-10-12 21:34:44 +1300

    Remove Redunant Import

--- apps/mobile/src/components/shared/ThemePopup.tsx
+++ apps/mobile/src/components/shared/ThemePopup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Popup from "../shared/Popup";
 import AchievementModel from "../../models/AchievementModel";
