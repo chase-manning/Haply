0a80dc3999c182663b49cc3d1267479637a0e717
Chase Manning <me@chasemanning.co.nz>
2020-10-12 13:31:07 +1300

    Change Settings to use Tag Popup Component

--- apps/mobile/src/components/tabs/Settings.tsx
+++ apps/mobile/src/components/tabs/Settings.tsx
@@ -1,9 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Popup from "../shared/Popup";
-import { SelectedTags, SelectedTag, Line } from "../../styles/Shared";
-import CloseIcon from "@material-ui/icons/Close";
-import AddIcon from "@material-ui/icons/Add";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../../state/userSlice";
 import {
@@ -12,19 +8,8 @@ import {
   toggleRemindersEnabled,
   toggleRandomReminders,
 } from "../../state/settingsSlice";
-import {
-  Mode,
-  selectColorPrimary,
-  toggleMode,
-  selectTagOptions,
-  selectMode,
-  removeTagOption,
-  addTagOption,
-} from "../../state/tempSlice";
-import {
-  selectAchievements,
-  selectDarkModeUnlocked,
-} from "../../state/dataSlice";
+import { Mode, toggleMode, selectMode } from "../../state/tempSlice";
+import { selectDarkModeUnlocked } from "../../state/dataSlice";
 import { showLogin } from "../../state/navigationSlice";
 import Setting from "../shared/Setting";
 import ReminderPopup from "../shared/ReminderPopup";
@@ -46,69 +31,12 @@ const Header = styled.div`
   color: var(--main);
 `;
 
-const PopupContent = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const TagIcon = styled.div`
-  font-size: 14px;
-  margin-left: 2px;
-  float: right;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const AddTag = styled.div`
-  height: 24px;
-  border-radius: 12px;
-  background-color: var(--sub-light);
-  color: var(--sub);
-  display: inline-block;
-  font-size: 14px;
-`;
-
-const AddTagContent = styled.button`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 8px;
-`;
-
-const AddTagText = styled.div`
-  font-size: 12px;
-  float: left;
-`;
-
-const TagInput = styled.input`
-  width: 100%;
-  border: solid 1px var(--border);
-  padding: 20px;
-  border-radius: 10px;
-  outline: none;
-  background-color: var(--bg-top);
-  color: var(--main);
-`;
-
-class State {
-  tagsPopupOpen: boolean = false;
-  newTag: string = "";
-  newTagPopupOpen: boolean = false;
-}
-
 const Settings = () => {
-  const [state, setState] = useState(new State());
   const dispatch = useDispatch();
   const user = useSelector(selectUser)!;
   const remindersEnabled = useSelector(selectRemindersEnabled)!;
   const randomReminders = useSelector(selectRandomReminders)!;
   const mode = useSelector(selectMode);
-  const tagOptions = useSelector(selectTagOptions);
   const darkModeUnlocked = useSelector(selectDarkModeUnlocked);
 
   return (
@@ -150,7 +78,7 @@ const Settings = () => {
           clickFunction={dispatch(toggleMode)}
         />
       )}
-      <Setting label={"Tags"} isToggle={false} popup={<div>TODO</div>} />
+      <Setting label={"Tags"} isToggle={false} popup={<TagPopup />} />
 
       <Header>Contact</Header>
       <Setting
@@ -191,69 +119,6 @@ const Settings = () => {
           )
         }
       />
-
-      {state.tagsPopupOpen && (
-        <Popup
-          content={
-            <PopupContent>
-              <SelectedTags>
-                {tagOptions.map((tagOption: string) => (
-                  <SelectedTag
-                    onClick={() => dispatch(removeTagOption(tagOption))}
-                    includeMargin={true}
-                  >
-                    {tagOption}
-                    <TagIcon>
-                      <CloseIcon fontSize={"inherit"} />
-                    </TagIcon>
-                  </SelectedTag>
-                ))}
-                <AddTag>
-                  <AddTagContent
-                    onClick={() =>
-                      setState({ ...state, newTagPopupOpen: true })
-                    }
-                  >
-                    <AddTagText>Add</AddTagText>
-                    <TagIcon>
-                      <AddIcon fontSize={"inherit"} />
-                    </TagIcon>
-                  </AddTagContent>
-                  {state.newTagPopupOpen && (
-                    <Popup
-                      content={
-                        <PopupContent>
-                          <TagInput
-                            value={state.newTag}
-                            placeholder="New Tag..."
-                            onChange={(event: any) =>
-                              setState({
-                                ...state,
-                                newTag: event.target.value,
-                              })
-                            }
-                          />
-                        </PopupContent>
-                      }
-                      showButton={true}
-                      close={() =>
-                        setState({
-                          ...state,
-                          newTagPopupOpen: false,
-                          newTag: "",
-                        })
-                      }
-                      submit={() => dispatch(addTagOption(state.newTag))}
-                    />
-                  )}
-                </AddTag>
-              </SelectedTags>
-            </PopupContent>
-          }
-          showButton={true}
-          close={() => setState({ ...state, tagsPopupOpen: false })}
-        />
-      )}
     </StyledSettings>
   );
 };
