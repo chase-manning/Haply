d8f664025f583d28a2d7d74a947f5d693fd77621
Chase Manning <me@chasemanning.co.nz>
2020-10-12 13:26:32 +1300

    Create Theme Popup

---
+++ apps/mobile/src/components/shared/ThemePopup.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Popup from "../shared/Popup";
+import AchievementModel from "../../models/AchievementModel";
+import { useDispatch, useSelector } from "react-redux";
+import { selectColorPrimary, setColorPrimary } from "../../state/tempSlice";
+import { selectAchievements } from "../../state/dataSlice";
+
+const PopupContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ColorOptions = styled.div`
+  width: 100%;
+  display: grid;
+  grid-row-gap: 10px;
+  align-items: center;
+  justify-items: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+type ColorOptionProps = {
+  selected: boolean;
+};
+
+const ColorOption = styled.button`
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: ${(props: ColorOptionProps) =>
+    props.selected ? "solid 2px var(--primary)" : "none"};
+`;
+
+type ColorProps = {
+  color: string;
+};
+
+const Color = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props: ColorProps) => props.color};
+`;
+
+const ThemePopup = () => {
+  const dispatch = useDispatch();
+  const colorPrimary = useSelector(selectColorPrimary);
+  const achievements = useSelector(selectAchievements);
+
+  return (
+    <Popup
+      content={
+        <PopupContent>
+          <ColorOptions>
+            {achievements
+              .filter(
+                (achievement: AchievementModel) =>
+                  achievement.colorPrimary !== "" &&
+                  achievement.percentComplete === 1
+              )
+              .map((achievement: AchievementModel) => (
+                <ColorOption
+                  onClick={() =>
+                    dispatch(setColorPrimary(achievement.colorPrimary))
+                  }
+                  selected={achievement.colorPrimary === colorPrimary}
+                >
+                  <Color color={achievement.colorPrimary} />
+                </ColorOption>
+              ))}
+          </ColorOptions>
+        </PopupContent>
+      }
+      showButton={true}
+      close={() => console.log("meow")}
+    />
+  );
+};
+
+export default ThemePopup;
