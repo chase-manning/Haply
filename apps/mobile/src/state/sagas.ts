36cbb5ecce7567e7e669145bba68f6e8f756ed7f
Chase Manning <me@chasemanning.co.nz>
2020-10-13 19:00:15 +1300

    Saving & Loading for Moods, Stats & Achievments

--- apps/mobile/src/state/sagas.ts
+++ apps/mobile/src/state/sagas.ts
@@ -1,7 +1,14 @@
 import { put, takeEvery, all, select, call } from "redux-saga/effects";
 import AchievementService from "../services/AchievementService";
 import StatService from "../services/StatService";
-import { selectMoods, setAchievements, setMoods, setStats } from "./dataSlice";
+import {
+  selectAchievements,
+  selectMoods,
+  selectStats,
+  setAchievements,
+  setMoods,
+  setStats,
+} from "./dataSlice";
 import { selectColorPrimary, selectMode } from "./tempSlice";
 
 import { Plugins as CapacitorPlugins } from "@capacitor/core";
@@ -16,6 +23,9 @@ import {
   updateNextNotification,
 } from "./settingsSlice";
 import { completeAppInit, initApp } from "./navigationSlice";
+import Mood from "../models/mood";
+import { StatModel } from "../models/StatModel";
+import AchievementModel from "../models/AchievementModel";
 const { Storage } = CapacitorPlugins;
 
 /* WATCHERS */
@@ -24,7 +34,18 @@ function* watchAppInit() {
 }
 
 function* watchSetMoods() {
-  yield takeEvery(setMoods, softUpdate);
+  yield takeEvery(setMoods, function* processSetMood() {
+    yield call(softUpdate);
+    yield call(saveMoods);
+  });
+}
+
+function* watchSetStats() {
+  yield takeEvery(setStats, saveStats);
+}
+
+function* watchSetAchievements() {
+  yield takeEvery(setAchievements, saveAchievements);
 }
 
 function* watchToggleRemindersEnabled() {
@@ -50,6 +71,9 @@ function* watchUpdateNextNotification() {
 /* ACTIONS */
 function* initialiseApp() {
   yield call(loadSettings);
+  yield call(loadMoods);
+  yield call(loadStats);
+  yield call(loadAchievements);
   yield put(completeAppInit());
 }
 
@@ -69,6 +93,26 @@ function* softUpdate() {
   yield put(setAchievements(achievements));
 }
 
+function* saveSettings() {
+  let settings: SettingsState = yield select(selectSettings);
+  Storage.set({ key: "settings", value: JSON.stringify(settings) });
+}
+
+function* saveMoods() {
+  let moods: Mood[] = yield select(selectMoods);
+  Storage.set({ key: "moods", value: JSON.stringify(moods) });
+}
+
+function* saveStats() {
+  let stats: StatModel[] = yield select(selectStats);
+  Storage.set({ key: "stats", value: JSON.stringify(stats) });
+}
+
+function* saveAchievements() {
+  let achievments: AchievementModel[] = yield select(selectAchievements);
+  Storage.set({ key: "achievements", value: JSON.stringify(achievments) });
+}
+
 function* loadSettings() {
   let ret: { value: any } = yield Storage.get({ key: "settings" });
   if (ret.value) {
@@ -77,15 +121,36 @@ function* loadSettings() {
   }
 }
 
-function* saveSettings() {
-  let settings: SettingsState = yield select(selectSettings);
-  Storage.set({ key: "settings", value: JSON.stringify(settings) });
+function* loadMoods() {
+  let ret: { value: any } = yield Storage.get({ key: "moods" });
+  if (ret.value) {
+    let moods: Mood[] = JSON.parse(ret.value);
+    yield put(setMoods(moods));
+  }
+}
+
+function* loadStats() {
+  let ret: { value: any } = yield Storage.get({ key: "stats" });
+  if (ret.value) {
+    let stats: StatModel[] = JSON.parse(ret.value);
+    yield put(setStats(stats));
+  }
+}
+
+function* loadAchievements() {
+  let ret: { value: any } = yield Storage.get({ key: "achievements" });
+  if (ret.value) {
+    let achievements: AchievementModel[] = JSON.parse(ret.value);
+    yield put(setAchievements(achievements));
+  }
 }
 
 export default function* rootSaga() {
   yield all([
     watchAppInit(),
     watchSetMoods(),
+    watchSetStats(),
+    watchSetAchievements(),
     watchToggleRemindersEnabled(),
     watchToggleRandomReminders(),
     watchSetFrequencyMinutesMin(),
