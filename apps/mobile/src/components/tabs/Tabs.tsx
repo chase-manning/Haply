77ec756a7c641395d7a0777528373954cd4f0ad6
Chase Manning <me@chasemanning.co.nz>
2020-10-14 21:41:40 +1300

    Convert Tabs to use User Store

--- apps/mobile/src/components/tabs/Tabs.tsx
+++ apps/mobile/src/components/tabs/Tabs.tsx
@@ -8,7 +8,6 @@ import Settings from "./Settings";
 import { Tab, selectActiveTab } from "../../state/navigationSlice";
 import { NOTFOUND } from "dns";
 import { useSelector } from "react-redux";
-import { User } from "firebase";
 
 const StyledTabs = styled.div`
   width: 100%;
@@ -16,20 +15,14 @@ const StyledTabs = styled.div`
   overflow: auto;
 `;
 
-type Props = {
-  user: User;
-};
-
-const Tabs = (props: Props) => {
+const Tabs = () => {
   const activeTab = useSelector(selectActiveTab);
 
   let tabContents;
   if (activeTab === Tab.Profile) tabContents = <Achievements />;
-  else if (activeTab === Tab.Entries)
-    tabContents = <Entries user={props.user} />;
+  else if (activeTab === Tab.Entries) tabContents = <Entries />;
   else if (activeTab === Tab.Stats) tabContents = <Analytics />;
-  else if (activeTab === Tab.Settings)
-    tabContents = <Settings user={props.user} />;
+  else if (activeTab === Tab.Settings) tabContents = <Settings />;
   else throw NOTFOUND;
 
   return <StyledTabs>{tabContents}</StyledTabs>;
