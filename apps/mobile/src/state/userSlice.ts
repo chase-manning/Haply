78890c2d10e7885e9cb35fe3688409b02b238993
Chase Manning <me@chasemanning.co.nz>
2020-10-14 21:39:15 +1300

    Add User to User Slice

--- apps/mobile/src/state/userSlice.ts
+++ apps/mobile/src/state/userSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "firebase";
 import { RootState } from "./store";
 
 /* TYPES */
 interface UserState {
+  user?: string;
   pushNotificationToken?: string;
 }
 
 const initialState: UserState = {
+  user: undefined,
   pushNotificationToken: undefined,
 };
 
@@ -15,16 +18,24 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = JSON.stringify(action.payload);
+    },
     setPushNotificationToken: (state, action: PayloadAction<string>) => {
       state.pushNotificationToken = action.payload;
     },
   },
 });
 
-export const { setPushNotificationToken } = userSlice.actions;
+export const { setPushNotificationToken, setUser } = userSlice.actions;
 
 /* SELECTS */
 export const selectPushNotificationToken = (state: RootState) =>
   state.user.pushNotificationToken;
+export const selectUser = (state: RootState) => {
+  if (!state.user.user) return undefined;
+  let user: User = JSON.parse(state.user.user!);
+  return user;
+};
 
 export default userSlice.reducer;
