f9d3e67949b407c501a8bfcdafa5c535cb198f9a
Chase Manning <me@chasemanning.co.nz>
2020-10-16 11:23:45 +1300

    Remove Redundnat Imports

--- apps/mobile/src/components/shared/StatChart.tsx
+++ apps/mobile/src/components/shared/StatChart.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import { DataPoint } from "../../models/StatModel";
 import { Line } from "react-chartjs-2";
