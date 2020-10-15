7a1e003b1622e01cc8e55e95c5251dcda431a3ec
Chase Manning <me@chasemanning.co.nz>
2020-10-16 11:23:31 +1300

    Remove Redundnat Imports

--- apps/mobile/src/components/shared/AchievementReward.tsx
+++ apps/mobile/src/components/shared/AchievementReward.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import DoneOutlinedIcon from "@material-ui/icons/DoneOutlined";
