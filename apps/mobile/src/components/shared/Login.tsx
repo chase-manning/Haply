7dd71ab056a1f7582a95e356a89337ebafbff57e
Chase Manning <me@chasemanning.co.nz>
2020-10-19 23:01:16 +1300

    Update Settings from Cloud on App Startup

--- apps/mobile/src/components/shared/Login.tsx
+++ apps/mobile/src/components/shared/Login.tsx
@@ -13,6 +13,7 @@ import {
   setIsAnonymous,
   setToken,
 } from "../../state/userSlice";
+import { updateSettings } from "../../state/settingsSlice";
 const { Storage } = CapacitorPlugins;
 
 const firebaseConfig = {
@@ -63,6 +64,7 @@ const Login = () => {
       dispatch(setIsAnonymous(changedUser.isAnonymous));
       dispatch(setToken(userToken));
       dispatch(updateMoods(userToken));
+      dispatch(updateSettings(userToken));
     });
 
     Storage.get({ key: "logged" }).then((result: any) => {
