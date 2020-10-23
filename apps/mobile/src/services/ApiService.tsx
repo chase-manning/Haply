142dc42cc40dfc390d55b328edf556687074341a
Chase Manning <me@chasemanning.co.nz>
2020-10-23 13:21:32 +1300

    Save New Token on Refresh

--- apps/mobile/src/services/ApiService.tsx
+++ apps/mobile/src/services/ApiService.tsx
@@ -1,10 +1,14 @@
+import { useDispatch } from "react-redux";
 import { firebaseApp } from "../components/shared/Login";
+import { setToken } from "../state/userSlice";
 
 const ApiService = async (route: string, requestOptions: any) => {
+  const dispatch = useDispatch();
   try {
     const response = await fetch(route, requestOptions);
     if (!response.ok) {
       const newToken = await firebaseApp.auth().currentUser!.getIdToken(true);
+      dispatch(setToken(newToken));
       requestOptions.headers.Authorization = "Bearer " + newToken;
       return await fetch(route, requestOptions);
     }
