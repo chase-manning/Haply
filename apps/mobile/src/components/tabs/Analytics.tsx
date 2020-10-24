e6f6759e76460eb73e9a9d09093524cd3b5db079
Chase Manning <me@chasemanning.co.nz>
2020-10-24 18:19:24 +1300

    Add Loading Line to Analytics

--- apps/mobile/src/components/tabs/Analytics.tsx
+++ apps/mobile/src/components/tabs/Analytics.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 import { StatModel } from "../../models/StatModel";
 import Stat from "../shared/Stat";
 import { useSelector } from "react-redux";
-import { selectStats } from "../../state/dataSlice";
+import { selectStats, selectStatsLoading } from "../../state/dataSlice";
+import LoadingLine from "../shared/LoadingLine";
 
 const StyledAnalytics = styled.div`
   width: 100%;
@@ -11,13 +12,16 @@ const StyledAnalytics = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 20px;
+  position: relative;
 `;
 
 const Analytics = () => {
   const stats = useSelector(selectStats);
+  const analyticsLoading = useSelector(selectStatsLoading);
 
   return (
     <StyledAnalytics>
+      <LoadingLine loading={analyticsLoading} />
       {stats.map((stat: StatModel) => (
         <Stat stat={stat} />
       ))}
