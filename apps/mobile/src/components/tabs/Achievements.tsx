1ffc43501d4a027384de5cb21c5e5e40c9e8bbb2
Chase Manning <me@chasemanning.co.nz>
2020-10-24 18:01:04 +1300

    Add Loading Line Component to Achievements

--- apps/mobile/src/components/tabs/Achievements.tsx
+++ apps/mobile/src/components/tabs/Achievements.tsx
@@ -4,19 +4,21 @@ import styled from "styled-components";
 import AchievementModel from "../../models/AchievementModel";
 import { selectAchievements } from "../../state/dataSlice";
 import Acheivement from "../shared/Achievement";
+import LoadingLine from "../shared/LoadingLine";
 
 const StyledAchievements = styled.div`
   width: 100%;
   padding: 20px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  position: relative;
 `;
-
 const Achievements = () => {
   const achievements = useSelector(selectAchievements);
 
   return (
     <StyledAchievements>
+      <LoadingLine />
       {achievements.map((achievment: AchievementModel) => (
         <Acheivement achievement={achievment} />
       ))}
