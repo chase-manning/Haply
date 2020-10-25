5edd31bdee97ae7504f96dcc51e2b1a1136255fe
Chase Manning <me@chasemanning.co.nz>
2020-10-25 13:44:28 +1300

    Add Content to Acheivement Alert

--- apps/mobile/src/components/shared/Alerts.tsx
+++ apps/mobile/src/components/shared/Alerts.tsx
@@ -3,10 +3,24 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import AchievementModel from "../../models/AchievementModel";
 import { selectAchievements } from "../../state/dataSlice";
-import Acheivement from "./Achievement";
+import AchievementPopupContent from "./AchievementPopupContent";
 import Popup from "./Popup";
 
 const StyledAlerts = styled.div``;
+
+const AchievmentAlertPopup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const AchievementAlertHeader = styled.div`
+  width: 100%;
+  display: flex;
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 18px;
+  color: var(--main);
+`;
 
 const Alerts = () => {
   const achievements = useSelector(selectAchievements);
@@ -18,9 +32,17 @@ const Alerts = () => {
     <StyledAlerts>
       <Popup
         open={newAchievements.length > 0}
-        content={<Acheivement achievement={newAchievements[0]} />}
+        content={
+          <AchievmentAlertPopup>
+            <AchievementAlertHeader>
+              Achievement Unlocked!
+            </AchievementAlertHeader>
+            <AchievementPopupContent achievement={newAchievements[0]} />
+          </AchievmentAlertPopup>
+        }
         showButton={true}
         close={() => console.log("meow")}
+        buttonText={"Awesome!"}
       />
     </StyledAlerts>
   );
