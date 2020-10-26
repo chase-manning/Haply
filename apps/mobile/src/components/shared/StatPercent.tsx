3c788ae41eef73ecf0daf0446c0a6d5c0435d089
Chase Manning <me@chasemanning.co.nz>
2020-10-26 22:07:16 +1300

    Formatting Improvements to Stat Percent Component

--- apps/mobile/src/components/shared/StatPercent.tsx
+++ apps/mobile/src/components/shared/StatPercent.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 const StyledStatPercent = styled.div`
   width: 100%;
@@ -10,24 +8,14 @@ const StyledStatPercent = styled.div`
   align-items: center;
 `;
 
-type ContentProps = {
+type PercentProps = {
   positive: boolean;
 };
 
-const Content = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: ${(props: ContentProps) =>
-    props.positive ? "var(--primary)" : "var(--highlight)"};
-`;
-
-const DirectionIndicator = styled.div`
-  margin-right: 20px;
-`;
-
 const Percent = styled.div`
-  font-size: 40px;
+  font-size: 80px;
+  color: ${(props: PercentProps) =>
+    props.positive ? "var(--primary)" : "var(--highlight)"};
 `;
 
 type Props = {
@@ -37,12 +25,9 @@ type Props = {
 const StatPercent = (props: Props) => {
   return (
     <StyledStatPercent>
-      <Content positive={props.percent >= 0}>
-        <DirectionIndicator>
-          {props.percent >= 0 ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-        </DirectionIndicator>
-        <Percent>{Math.round(props.percent * 100) + "%"} </Percent>
-      </Content>
+      <Percent positive={props.percent >= 0}>
+        {Math.round(props.percent * 100) + "%"}{" "}
+      </Percent>
     </StyledStatPercent>
   );
 };
