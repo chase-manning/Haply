3cb12582766a73c9676ae1a29ea598da7af36988
Chase Manning <me@chasemanning.co.nz>
2020-10-26 17:47:08 +1300

    Add New Flag in Stats Service

--- apps/mobile/src/services/StatService.tsx
+++ apps/mobile/src/services/StatService.tsx
@@ -5,7 +5,10 @@ const api: string =
   "https://us-central1-happiness-software.cloudfunctions.net/webApi/api/";
 
 const StatService = {
-  async getStats(userToken: string): Promise<StatModel[] | null> {
+  async getStats(
+    userToken: string,
+    currentStats: StatModel[]
+  ): Promise<StatModel[] | null> {
     try {
       const route: string = api + "v2/stats";
 
@@ -18,7 +21,9 @@ const StatService = {
       const response = await ApiService(route, requestOptions);
       if (!response) return null;
 
-      const stats: StatModel[] = await response!.json();
+      let stats: StatModel[] = await response!.json();
+
+      stats = getStatsWithIsNew(currentStats, stats);
 
       return stats;
     } catch (error) {
@@ -26,6 +31,30 @@ const StatService = {
       return null;
     }
   },
+};
+
+const getStatsWithIsNew = (
+  currentStats: StatModel[],
+  newStats: StatModel[]
+): StatModel[] => {
+  if (currentStats.length === 0) return newStats;
+  newStats
+    .filter((newStat: StatModel) => newStat.percentComplete === 1)
+    .forEach((newStat: StatModel) => {
+      let currentStatList = currentStats.filter(
+        (stat: StatModel) => stat.title === newStat.title
+      );
+      if (currentStatList.length > 0) {
+        let currentStat = currentStatList[0];
+        if (
+          currentStat &&
+          currentStat.percentComplete < 1 &&
+          newStat.percentComplete === 1
+        )
+          newStat.isNew = true;
+      }
+    });
+  return newStats;
 };
 
 export default StatService;
