69496292ac37cf2202012489e0b066426a473a6a
Chase Manning <me@chasemanning.co.nz>
2020-10-28 17:02:55 +1300

    Remove Redundant Animations

--- apps/mobile/src/components/shared/NavBar.tsx
+++ apps/mobile/src/components/shared/NavBar.tsx
@@ -26,12 +26,6 @@ const StyledNavBar = styled.div`
   box-shadow: var(--shadow);
   margin: 10px;
   border-radius: 15px;
-  transition: all 0.2s ease-out;
-
-  &:active:hover {
-    transform: scale(0.99);
-    box-shadow: var(--shadow-clicked);
-  }
 `;
 
 type NavItemProps = {
