49f4ea5fcf7cb54fc1f4bd6ab7b27d9ac16f5dee
Chase Manning <me@chasemanning.co.nz>
2020-10-31 21:22:19 +1300

    Add Product Price to Premium Reducer

--- apps/mobile/src/state/premiumSlice.tsx
+++ apps/mobile/src/state/premiumSlice.tsx
@@ -1,13 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 
 /* TYPES */
+type Product = {
+  price: string;
+};
+
 interface PremiumState {
   isPremium: boolean;
+  product: Product;
 }
 
 const initialState: PremiumState = {
   isPremium: false,
+  product: { price: "" },
 };
 
 /* SLICE */
@@ -21,12 +27,17 @@ export const premiumSlice = createSlice({
     setFree: (state) => {
       state.isPremium = false;
     },
+    setProductPrice: (state, action: PayloadAction<string>) => {
+      state.product.price = action.payload;
+    },
   },
 });
 
-export const { setPremium, setFree } = premiumSlice.actions;
+export const { setPremium, setFree, setProductPrice } = premiumSlice.actions;
 
 /* SELECTS */
 export const selectIsPremium = (state: RootState) => state.premium.isPremium;
+export const selectProductPrice = (state: RootState) =>
+  state.premium.product.price;
 
 export default premiumSlice.reducer;
