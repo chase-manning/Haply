0dc405a338650defe931a4d07a0a97dcbfb83008
Chase Manning <me@chasemanning.co.nz>
2020-11-01 14:24:10 +1300

    Implement React Router on Website

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TermsAndConditions from "./components/TermsAndConditions";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 const StyledApp = styled.div`
   width: 100%;
@@ -10,6 +13,19 @@ export default class App extends Component {
   render() {
     return (
       <StyledApp>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/privacy-policy">
+              <PrivacyPolicy />
+            </Route>
+            <Route path="/terms-and-conditions">
+              <TermsAndConditions />
+            </Route>
+          </Switch>
+        </Router>
         <Home />
       </StyledApp>
     );
