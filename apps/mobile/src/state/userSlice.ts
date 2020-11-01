15db6c6ba7a99d842eba84120a4a1a70ff7d2a83
Chase Manning <me@chasemanning.co.nz>
2020-11-01 21:24:02 +1300

    Return Empty String for Token When Expired

--- apps/mobile/src/state/userSlice.ts
+++ apps/mobile/src/state/userSlice.ts
@@ -4,6 +4,7 @@ import { RootState } from "./store";
 /* TYPES */
 interface UserState {
   token: string;
+  tokenUpdated: Date;
   isAnonymous: boolean;
   id: string;
   pushNotificationToken?: string;
@@ -11,6 +12,7 @@ interface UserState {
 
 const initialState: UserState = {
   token: "",
+  tokenUpdated: new Date(),
   isAnonymous: true,
   id: "",
   pushNotificationToken: undefined,
@@ -23,6 +25,7 @@ export const userSlice = createSlice({
   reducers: {
     setToken: (state, action: PayloadAction<string>) => {
       state.token = action.payload;
+      state.tokenUpdated = new Date();
     },
     setIsAnonymous: (state, action: PayloadAction<boolean>) => {
       state.isAnonymous = action.payload;
@@ -47,8 +50,15 @@ export const {
 export const selectUser = (state: RootState) => state.user;
 export const selectPushNotificationToken = (state: RootState) =>
   state.user.pushNotificationToken;
-export const selectToken = (state: RootState) => state.user.token;
+export const selectToken = (state: RootState) => {
+  const now = new Date();
+  const tokenAge = now.getTime() - state.user.tokenUpdated.getTime();
+  const tokenExpired = tokenAge / (1000 * 60 * 60) >= 1;
+  if (tokenExpired) return "";
+  return state.user.token;
+};
 export const selectIsAnonymous = (state: RootState) => state.user.isAnonymous;
 export const selectId = (state: RootState) => state.user.id;
+export const selectTokenExpired = (state: RootState) => {};
 
 export default userSlice.reducer;
