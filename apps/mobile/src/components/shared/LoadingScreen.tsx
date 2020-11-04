c1c42f06ecc5c0961716cfdea7350dfce96a1af6
Chase Manning <me@chasemanning.co.nz>
2020-11-04 20:30:09 +1300

    Change Loading Screen Background to White

--- apps/mobile/src/components/shared/LoadingScreen.tsx
+++ apps/mobile/src/components/shared/LoadingScreen.tsx
@@ -8,7 +8,7 @@ const StyledLoadingScreen = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: var(--bg);
+  background-color: var(--bg-mid);
   display: flex;
   justify-content: center;
   align-items: center;
