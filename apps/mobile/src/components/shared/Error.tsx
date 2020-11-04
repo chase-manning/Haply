ef81966e5bff5e4709c8b44006a43a160a746970
Chase Manning <me@chasemanning.co.nz>
2020-11-04 18:36:44 +1300

    Center Align Error Content

--- apps/mobile/src/components/shared/Error.tsx
+++ apps/mobile/src/components/shared/Error.tsx
@@ -9,6 +9,8 @@ const PopupContent = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Svg = styled.img`
@@ -18,13 +20,13 @@ const Svg = styled.img`
 const Title = styled.div`
   text-align: center;
   width: 100%;
-  font-size: 16px;
+  font-size: 18px;
   color: var(--main);
   margin-top: 30px;
 `;
 
 const Description = styled.div`
-  font-size: 12px;
+  font-size: 14px;
   margin: 10px 0 12px 0;
   color: var(--sub);
   width: 70%;
