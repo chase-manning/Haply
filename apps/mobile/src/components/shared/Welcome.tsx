9d7ff8faca7173c400a18560c48129976be308a0
Chase Manning <me@chasemanning.co.nz>
2020-11-05 17:06:39 +1300

    Create Welcome Component

---
+++ apps/mobile/src/components/shared/Welcome.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectShowWelcome,
+  showWelcome,
+  hideWelcome,
+} from "../../state/navigationSlice";
+import { Plugins as CapacitorPlugins } from "@capacitor/core";
+const { Storage } = CapacitorPlugins;
+
+const StyledWelcome = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: var(--bg-mid);
+`;
+
+const Welcome = () => {
+  const dispatch = useDispatch();
+  const show = useSelector(selectShowWelcome);
+
+  useEffect(() => {
+    Storage.get({ key: "welcomed" }).then((result: any) => {
+      let ret: { value: any } = result;
+      if (ret.value) {
+        dispatch(hideWelcome());
+      } else {
+        dispatch(showWelcome());
+      }
+    });
+  }, []);
+
+  if (!show) return null;
+
+  return <StyledWelcome>Meow</StyledWelcome>;
+};
+
+export default Welcome;
