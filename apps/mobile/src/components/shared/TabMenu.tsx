d61a1c44aab2bc9b8174725799a90abe80df6d5e
Chase Manning <me@chasemanning.co.nz>
2020-11-07 19:23:13 +1300

    Add Selected Styling for Tab Headers

--- apps/mobile/src/components/shared/TabMenu.tsx
+++ apps/mobile/src/components/shared/TabMenu.tsx
@@ -11,8 +11,21 @@ const Tabs = styled.div`
   display: flex;
 `;
 
+type TabProps = {
+  active: boolean;
+};
+
 const Tab = styled.button`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--bg-mid);
+  padding: 20px;
+  color: ${(props: TabProps) =>
+    props.active ? "var(--primary)" : "var(--main)"};
+  border-bottom: ${(props: TabProps) =>
+    props.active ? "solid 4px var(--primary)" : "solid 4px var(--bg-mid)"};
 `;
 
 const Content = styled.div`
@@ -40,7 +53,10 @@ const TabMenu = (props: Props) => {
     <StyledTabMenu>
       <Tabs>
         {props.tabs.map((tab: Tab, index: number) => (
-          <Tab onClick={() => setState({ ...state, tab: index })}>
+          <Tab
+            onClick={() => setState({ ...state, tab: index })}
+            active={state.tab === index}
+          >
             {tab.title}
           </Tab>
         ))}
