5073fb548064a502902dd7ac43309f30400a056c
Chase Manning <me@chasemanning.co.nz>
2020-11-08 09:50:58 +1300

    Add Padding to Calander Tab

--- apps/mobile/src/components/tabs/Calendar.tsx
+++ apps/mobile/src/components/tabs/Calendar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { Header } from "../../styles/Shared";
 
 const StyledCalendar = styled.div`
   width: 100%;
   display: flex;
+  padding: 30px;
 `;
 
 const Calendar = () => {
-  return <StyledCalendar>meow</StyledCalendar>;
+  return (
+    <StyledCalendar>
+      <Header></Header>
+    </StyledCalendar>
+  );
 };
 
 export default Calendar;
