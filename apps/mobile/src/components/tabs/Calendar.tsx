08b96890893f3f5b2827688c577ac9f47e808610
Chase Manning <me@chasemanning.co.nz>
2020-11-07 22:23:29 +1300

    Create Calendar Component

---
+++ apps/mobile/src/components/tabs/Calendar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledCalendar = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const Calendar = () => {
+  return <StyledCalendar>meow</StyledCalendar>;
+};
+
+export default Calendar;
