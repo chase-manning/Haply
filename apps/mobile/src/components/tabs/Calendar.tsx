91d5106ea8d4ccb1eb99331b0d124c21b6055c85
Chase Manning <me@chasemanning.co.nz>
2020-11-08 14:25:46 +1300

    Change Calendar to Have 6 Columns

--- apps/mobile/src/components/tabs/Calendar.tsx
+++ apps/mobile/src/components/tabs/Calendar.tsx
@@ -29,7 +29,7 @@ const MonthSection = styled.div`
 const Dates = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   grid-row-gap: 15px;
   justify-items: center;
 `;
