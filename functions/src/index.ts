e29a9fc5ca2b6ec834a9a607582e9980aab1f627
Chase Manning <me@chasemanning.co.nz>
2020-11-09 19:35:26 +1300

    Create Updated Mood API for Getting by Date

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -312,6 +312,112 @@ app.get("/moods/:id", async (request, response) => {
   }
 });
 
+app.get("/v2/moods", async (request: any, response) => {
+  try {
+    const user = await getUser(request);
+    if (!user) return response.status(403).send("Unauthorized");
+    const userId = user.uid;
+
+    const order: string = request.query.order;
+    const limit: number = request.query.limit;
+    const startDate: string = request.query.startdate;
+    const endDate: string = request.query.enddate;
+
+    console.log("start Date");
+    console.log(startDate);
+    console.log(new Date(startDate));
+    console.log("end date");
+    console.log(endDate);
+    console.log(new Date(endDate));
+
+    let moodQuerySnapshot;
+    if (order) {
+      if (limit) {
+        if (startDate && endDate) {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .where("date", ">=", new Date(startDate).toISOString())
+            .where("date", "<=", new Date(endDate).toISOString())
+            .orderBy(order, "desc")
+            .limit(32)
+            .get();
+        } else {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .orderBy(order, "desc")
+            .limit(32)
+            .get();
+        }
+      } else {
+        if (startDate && endDate) {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .where("date", ">=", new Date(startDate).toISOString())
+            .where("date", "<=", new Date(endDate).toISOString())
+            .orderBy(order, "desc")
+            .get();
+        } else {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .orderBy(order, "desc")
+            .get();
+        }
+      }
+    } else {
+      if (limit) {
+        if (startDate && endDate) {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .where("date", ">=", new Date(startDate).toISOString())
+            .where("date", "<=", new Date(endDate).toISOString())
+            .limit(32)
+            .get();
+        } else {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .limit(32)
+            .get();
+        }
+      } else {
+        if (startDate && endDate) {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .where("date", ">=", new Date(startDate).toISOString())
+            .where("date", "<=", new Date(endDate).toISOString())
+            .get();
+        } else {
+          moodQuerySnapshot = await db
+            .collection("moods")
+            .where("userId", "==", userId)
+            .get();
+        }
+      }
+    }
+
+    console.log(moodQuerySnapshot);
+
+    const moods: any[] = [];
+    moodQuerySnapshot.forEach((doc) => {
+      moods.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+
+    return response.json(moods);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send(error);
+  }
+});
+
 app.get("/moods", async (request: any, response) => {
   try {
     const user = await getUser(request);
