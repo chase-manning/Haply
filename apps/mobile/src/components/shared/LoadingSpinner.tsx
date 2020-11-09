a9781798df03e4f7d7d0206ad327eb6abf1eabac
Chase Manning <me@chasemanning.co.nz>
2020-11-09 19:52:58 +1300

    Create Loading Spinner Component

---
+++ apps/mobile/src/components/shared/LoadingSpinner.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { CircularProgress, makeStyles } from "@material-ui/core";
+import styled from "styled-components";
+
+const StyledLoadingSpinner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+type Props = {
+  loading: boolean;
+};
+
+const LoadingSpinner = (props: Props) => {
+  const useStylesHaply = makeStyles(() => ({
+    top: {
+      color: "var(--primary)",
+    },
+    circle: {
+      strokeLinecap: "round",
+    },
+  }));
+  const classes = useStylesHaply();
+
+  if (!props.loading) return null;
+  return (
+    <StyledLoadingSpinner>
+      <CircularProgress
+        variant="indeterminate"
+        className={classes.top}
+        classes={{
+          circle: classes.circle,
+        }}
+        size={40}
+        thickness={4}
+        {...props}
+      />
+    </StyledLoadingSpinner>
+  );
+};
+
+export default LoadingSpinner;
