0ed00c57ec5a58e330e3ccf6a8a7e6982f6009af
Chase Manning <me@chasemanning.co.nz>
2020-11-11 20:57:50 +1300

    Create Pixels Component

---
+++ apps/mobile/src/components/shared/Pixels.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPixels = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+`;
+
+const Pixels = () => {
+  return <StyledPixels>Meow</StyledPixels>;
+};
+
+export default Pixels;
