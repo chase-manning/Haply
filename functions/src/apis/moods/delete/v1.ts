2d606e989b47d009ecfea986fc7ed414e477bca4
Chase Manning <me@chasemanning.co.nz>
2020-11-20 15:46:10 +1300

    :sparkles: Create Mood Delete V1 API

---
+++ functions/src/apis/moods/delete/v1.ts
@@ -0,0 +1,34 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import { getUser } from "../../../get-user";
+
+const firebase = admin.initializeApp(
+  functions.config().firebase,
+  "apisMoodsCreateV1"
+);
+const db = firebase.firestore();
+const app = express();
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+
+app.delete("/moods/:id", async (request, response) => {
+  try {
+    const user = await getUser(request);
+    if (!user) return response.status(403).send("Unauthorized");
+
+    const moodId = request.params.id;
+
+    if (!moodId) throw new Error("id is blank");
+
+    await db.collection("moods").doc(moodId).delete();
+
+    return response.json({
+      id: moodId,
+    });
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
