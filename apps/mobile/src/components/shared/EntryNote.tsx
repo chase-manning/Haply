7676386e8d10ec9ba635d66069486f1b4e2f4391
Chase Manning <me@chasemanning.co.nz>
2020-11-21 12:20:06 +1300

    :lipstick: Decrease Spacing Between Tags and Note

--- apps/mobile/src/components/shared/EntryNote.tsx
+++ apps/mobile/src/components/shared/EntryNote.tsx
@@ -9,7 +9,7 @@ type EntryNoteProps = {
 const StyledEntryNote = styled.div`
   color: var(--sub);
   font-size: 12px;
-  margin-top: 15px;
+  margin-top: 10px;
   width: 100%;
   height: ${(props: EntryNoteProps) => (props.open ? "auto" : "26px")};
   overflow: hidden;
