b22c5f45064175c8a0ba46635d7ffa8cccfffe22
Chase Manning <me@chasemanning.co.nz>
2020-11-20 19:19:07 +1300

    :fire: Remove Redundant Imports from Tag Input

--- apps/mobile/src/components/shared/TagInput.tsx
+++ apps/mobile/src/components/shared/TagInput.tsx
@@ -1,76 +1,5 @@
 import React, { useState } from "react";
-import Mood, { moodDescriptions } from "../../models/mood";
 import styled from "styled-components";
-import MoodService from "../../services/MoodService";
-import sadAsset from "../../assets/svgs/FeelingBlue.svg";
-import happyAsset from "../../assets/svgs/SmileyFace.svg";
-import okayAsset from "../../assets/svgs/YoungAndHappy.svg";
-import mehAsset from "../../assets/svgs/WindyDay.svg";
-import MoodSlider from "../shared/MoodSlider";
-import AddNote from "../shared/AddNote";
-import AddTags from "../shared/AddTags";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  hideMood,
-  selectMoodShowing,
-  selectMoodDateSearch,
-} from "../../state/navigationSlice";
-import { addMood, selectMoods } from "../../state/dataSlice";
-import { updateAll, updateDateSearchMoods } from "../../state/loadingSlice";
-import { selectUser } from "../../state/userSlice";
-import ExitBar from "../shared/ExitBar";
-import { Button } from "../../styles/Shared";
-import {
-  selectActivities,
-  selectFeelings,
-  selectPeople,
-  selectPlaces,
-} from "../../state/settingsSlice";
-
-const StyledCreateMood = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: var(--bg);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 30px;
-  color: var(--main);
-`;
-
-const Header = styled.div`
-  font-size: 40px;
-  text-align: center;
-  font-weight: 500;
-  color: var(--main);
-`;
-
-const Emotion = styled.div`
-  font-weight: 400;
-  font-size: 20px;
-  text-align: center;
-  color: var(--sub);
-`;
-
-const Tags = styled.div`
-  width: 100%;
-  font-size: 24px;
-`;
-
-const TagSection = styled.div`
-  display: inline-block;
-  margin-bottom: 10px;
-`;
-
-const TagText = styled.div`
-  color: var(--main);
-  display: inline-block;
-  margin-right: 10px;
-`;
 
 const TagSelected = styled.div`
   color: var(--primary);
@@ -121,21 +50,6 @@ const TagExit = styled.div`
   height: 100%;
   z-index: 1;
   background-color: var(--main-light);
-`;
-
-const Face = styled.div`
-  height: 300px;
-  width: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Additions = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
 `;
 
 class State {
