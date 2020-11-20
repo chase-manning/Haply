72c84d1969e730dd1e6b0fffba45df0bcac0146c
Chase Manning <me@chasemanning.co.nz>
2020-11-20 15:52:50 +1300

    :sparkles: Add Mood Update V1 API

---
+++ functions/src/apis/moods/update/v1.ts
@@ -0,0 +1,44 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import { getUser } from "../../../get-user";
+
+const firebase = admin.initializeApp(
+  functions.config().firebase,
+  "moodsUpdateV1"
+);
+const db = firebase.firestore();
+const app = express();
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+
+app.put("/moods/:id", async (request, response) => {
+  try {
+    const user = await getUser(request);
+    if (!user) return response.status(403).send("Unauthorized");
+
+    const moodId = request.params.id;
+    const { value, date, note, tags } = request.body;
+    const data = {
+      value,
+      date,
+      tags,
+      note,
+    };
+
+    if (!moodId) throw new Error("id is blank");
+
+    await db.collection("moods").doc(moodId).set(data, { merge: true });
+
+    return response.json({
+      id: moodId,
+      data,
+    });
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
+
+export const moodsUpdateV1 = app;
