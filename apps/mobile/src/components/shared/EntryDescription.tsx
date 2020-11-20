2f9201b30521225f3d86232f571affc472e08ff7
Chase Manning <me@chasemanning.co.nz>
2020-11-21 11:32:04 +1300

    :rocket: Add Tag Text to Entry Description

--- apps/mobile/src/components/shared/EntryDescription.tsx
+++ apps/mobile/src/components/shared/EntryDescription.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 import Mood from "../../models/mood";
+import { getTagText } from "./TagSelector";
 
 const StyledEntryDescription = styled.div`
   width: 100%;
-  font-size: 16px;
+  font-size: 12px;
+  margin-top: 10px;
+`;
+
+const Section = styled.div`
+  display: inline-block;
 `;
 
 const Text = styled.div`
   color: var(--main);
   display: inline-block;
-  margin-right: 10px;
+  margin-right: 3px;
 `;
 
 const Tag = styled.div`
   color: var(--primary);
   display: inline-block;
-  min-width: 50px;
-  min-height: 28px;
-  border-bottom: solid 2px var(--primary);
-  margin-right: 10px;
+  margin-right: 3px;
   position: relative;
 `;
 
@@ -28,7 +31,43 @@ type Props = {
 };
 
 const EntryDescription = (props: Props) => {
-  return <StyledEntryDescription></StyledEntryDescription>;
+  const hasFeelings = props.mood.feelings && props.mood.feelings.length > 0;
+  const hasActivities =
+    props.mood.activities && props.mood.activities.length > 0;
+  const hasPlaces = props.mood.places && props.mood.places.length > 0;
+  const hasPeople = props.mood.people && props.mood.people.length > 0;
+
+  if (!hasFeelings && !hasActivities && !hasPlaces && !hasPeople) return null;
+
+  return (
+    <StyledEntryDescription>
+      <Text>I'm </Text>
+      {hasFeelings && (
+        <Section>
+          <Text>feeling</Text>
+          <Tag>{getTagText(props.mood.feelings)}</Tag>
+        </Section>
+      )}
+      {hasActivities && (
+        <Section>
+          <Text>{hasFeelings ? "while" : ""}</Text>
+          <Tag>{getTagText(props.mood.activities)}</Tag>
+        </Section>
+      )}
+      {hasPlaces && (
+        <Section>
+          <Text>at</Text>
+          <Tag>{getTagText(props.mood.places)}</Tag>
+        </Section>
+      )}
+      {hasPeople && (
+        <Section>
+          <Text>with</Text>
+          <Tag>{getTagText(props.mood.people)}</Tag>
+        </Section>
+      )}
+    </StyledEntryDescription>
+  );
 };
 
 export default EntryDescription;
