b4056e204e77b32d86475884bbb0619c7e774b52
Chase Manning <me@chasemanning.co.nz>
2020-11-20 16:00:23 +1300

    :bug: Fix Issue with Moods Update Path

--- functions/src/apis/moods/update/v1.ts
+++ functions/src/apis/moods/update/v1.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 
-app.put("/moods/:id", async (request, response) => {
+app.put("/:id", async (request, response) => {
   try {
     const user = await getUser(request);
     if (!user) return response.status(403).send("Unauthorized");
