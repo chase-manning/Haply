2525b0e513d86f10d056a7136b649bbe9537009f
Chase Manning <me@chasemanning.co.nz>
2020-11-21 10:25:48 +1300

    :fire: Remove Redundant Imports from Entry

--- apps/mobile/src/components/shared/Entry.tsx
+++ apps/mobile/src/components/shared/Entry.tsx
@@ -1,16 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import dateFormat from "dateformat";
-import MoodService from "../../services/MoodService";
 import Mood from "../../models/mood";
 import { Card } from "../../styles/Shared";
-import { useDispatch, useSelector } from "react-redux";
-import { removeMood } from "../../state/dataSlice";
-import { updateAll } from "../../state/loadingSlice";
-import { selectToken } from "../../state/userSlice";
-import DynamicIcon from "./DynamicIcon";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
 import EntryTags from "./EntryTags";
 import EntryNote from "./EntryNote";
 import EntryHeader from "./EntryHeader";
@@ -26,84 +17,11 @@ const EntryContent = styled.div`
   align-items: center;
 `;
 
-const Header = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-const HeaderLeft = styled.div`
-  display: flex;
-`;
-
-const EntryText = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  text-align: left;
-  margin-left: 15px;
-`;
-
-const EntrySubHeader = styled.div`
-  color: var(--sub);
-  font-size: 11px;
-`;
-
-const KebabMenu = styled.button`
-  color: var(--sub);
-  position: relative;
-`;
-
-const ContextMenu = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, 15px);
-  background-color: var(--bg-mid);
-  border-radius: 6px;
-  display: flex;
-  z-index: 1;
-`;
-
-const ContextColor = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border-radius: 6px;
-  background-color: var(--sub-light);
-`;
-
-const ContentItem = styled.button`
-  display: flex;
-  color: var(--main);
-  justify-content: center;
-  align-items: center;
-  font-size: 12px;
-  padding: 2px;
-`;
-
-const ContextIcon = styled.div`
-  transform: scale(0.65);
-`;
-
-const ItemLabel = styled.div`
-  margin-right: 10px;
-`;
-
-class State {
-  popupOpen: boolean = false;
-}
-
 type Props = {
   mood: Mood;
 };
 
 const Entry = (props: Props) => {
-  const [state, setState] = useState(new State());
-  const dispatch = useDispatch();
-  const userToken = useSelector(selectToken);
-
   return (
     <StyledEntry>
       <Card>
