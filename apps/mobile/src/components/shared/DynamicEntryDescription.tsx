d7449f06bdecbca6b763292461f1c3abbbdc03c5
Chase Manning <me@chasemanning.co.nz>
2020-11-21 12:06:09 +1300

    :sparkles: Add Dynamic Entry Description Component

---
+++ apps/mobile/src/components/shared/DynamicEntryDescription.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Mood from "../../models/mood";
+import EntryDescription from "./EntryDescription";
+
+const StyledDynamicEntryDescription = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+type Props = {
+  mood: Mood;
+};
+const DynamicEntryDescription = (props: Props) => {
+  return (
+    <StyledDynamicEntryDescription>
+      <EntryDescription mood={props.mood} />
+      <EntryDescription
+        mood={props.mood}
+        opacity={props.mood.value / 10}
+        overlay={true}
+      />
+    </StyledDynamicEntryDescription>
+  );
+};
+
+export default DynamicEntryDescription;
