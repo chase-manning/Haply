d556680de81e9eab6d02b661a03e0da7901a280b
Chase Manning <me@chasemanning.co.nz>
2020-11-21 10:35:33 +1300

    :sparkles: Add Text and Tag Styled Components

--- apps/mobile/src/components/shared/EntryDescription.tsx
+++ apps/mobile/src/components/shared/EntryDescription.tsx
@@ -4,6 +4,23 @@ import Mood from "../../models/mood";
 
 const StyledEntryDescription = styled.div`
   width: 100%;
+  font-size: 16px;
+`;
+
+const Text = styled.div`
+  color: var(--main);
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+const Tag = styled.div`
+  color: var(--primary);
+  display: inline-block;
+  min-width: 50px;
+  min-height: 28px;
+  border-bottom: solid 2px var(--primary);
+  margin-right: 10px;
+  position: relative;
 `;
 
 type Props = {
