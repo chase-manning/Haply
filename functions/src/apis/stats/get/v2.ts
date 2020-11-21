f1345bf04ad4319aef72c4224442a0ebcc86a983
Chase Manning <me@chasemanning.co.nz>
2020-11-21 21:16:09 +1300

    :lipstick: Update Stat Comparison Unlock Wording

--- functions/src/apis/stats/get/v2.ts
+++ functions/src/apis/stats/get/v2.ts
@@ -278,7 +278,7 @@ const createStatComparison = (
     title: "Mood by " + tagName,
     type: StatType.Comparison,
     locked: validDataPoints.length < 3,
-    lockedMessage: "Record 3 different " + tagType + " to unlock",
+    lockedMessage: "Record a mood with 3 " + tagType + " to unlock",
     percentComplete: Math.min(validDataPoints.length / 3, 1),
     dataPoints: validDataPoints,
   };
