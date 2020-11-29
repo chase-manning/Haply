bac41074ae72ad312cdf959d623a444af40d0298
Chase Manning <me@chasemanning.co.nz>
2020-11-29 19:37:45 +1300

    :lipstick: Add Title and Description to Failed

--- apps/mobile/src/components/shared/Failed.tsx
+++ apps/mobile/src/components/shared/Failed.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Header } from "../../styles/Shared";
 import Popup from "./Popup";
 import notify from "../../assets/svgs/Notify.svg";
 
@@ -8,10 +7,29 @@ const Content = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Svg = styled.img`
   width: 80%;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  width: 100%;
+  font-size: 18px;
+  color: var(--main);
+  margin-top: 30px;
+`;
+
+const Description = styled.div`
+  font-size: 14px;
+  margin: 10px 0 12px 0;
+  color: var(--sub);
+  width: 100%;
+  text-align: center;
+  line-height: 1.5;
 `;
 
 type Props = {
@@ -24,12 +42,15 @@ type Props = {
 const Failed = (props: Props) => {
   return (
     <Popup
-      open={props.open}
+      open={true}
       close={() => props.close()}
       content={
         <Content>
-          <Header>{props.action + " Failed"}</Header>
           <Svg src={notify}></Svg>
+          <Title>{props.action + " Failed"}</Title>
+          <Description>
+            {"Ensure you have internet connection and try again"}
+          </Description>
         </Content>
       }
       submit={() => props.tryAgain()}
