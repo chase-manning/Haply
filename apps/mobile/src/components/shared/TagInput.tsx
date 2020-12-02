53da98ae437c2b7101191569c875f4e8d8f92056
Chase Manning <me@chasemanning.co.nz>
2020-12-02 21:44:29 +1300

    :fire: Remove Redundant Styles

--- apps/mobile/src/components/shared/TagInput.tsx
+++ apps/mobile/src/components/shared/TagInput.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import CheckIcon from "@material-ui/icons/Check";
 import ContextMenu from "./ContextMenu";
-import TagSelector from "./TagSelector";
 
 const StyledTagInput = styled.div`
   color: var(--primary);
@@ -20,62 +18,6 @@ const ClickHandler = styled.button`
   top: 0;
   left: 0;
   position: absolute;
-`;
-
-const Options = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: var(--bg-mid);
-  border-radius: 10px;
-  box-shadow: var(--shadow);
-  display: flex;
-  flex-direction: column;
-  padding: 10px 15px;
-  z-index: 2;
-  font-size: 16px;
-`;
-
-const Option = styled.div`
-  padding: 5px 0;
-  display: flex;
-  align-items: center;
-`;
-
-type OptionIconProps = {
-  selected: boolean;
-};
-
-const OptionIcon = styled.div`
-  color: ${(props: OptionIconProps) =>
-    props.selected ? "var(--primary)" : "var(--bg-mid)"};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-right: 5px;
-`;
-
-type OptionTextProps = {
-  selected: boolean;
-};
-
-const OptionText = styled.div`
-  color: ${(props: OptionTextProps) =>
-    props.selected ? "var(--primary)" : "var(--main)"};
-  white-space: nowrap;
-  display: flex;
-  align-items: center;
-`;
-
-const Exit = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-  background-color: var(--main-light);
 `;
 
 class State {
