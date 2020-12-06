d86e4acecc9e5caa26b471221049c07c6afc9ec2
Chase Manning <me@chasemanning.co.nz>
2020-12-06 16:38:31 +1300

    :sparkles: Add Border Radius to Loading Spinner

--- apps/mobile/src/components/shared/LoadingSpinner.tsx
+++ apps/mobile/src/components/shared/LoadingSpinner.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import LoadingCircle from "./LoadingCircle";
 
+type SpinnerProps = {
+  borderRadius: number;
+};
+
 const StyledLoadingSpinner = styled.div`
   position: absolute;
   top: 0;
@@ -12,16 +16,18 @@ const StyledLoadingSpinner = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius: ${(props: SpinnerProps) => props.borderRadius + "px"};
 `;
 
 type Props = {
   loading: boolean;
+  borderRadius?: number;
 };
 
 const LoadingSpinner = (props: Props) => {
   if (!props.loading) return null;
   return (
-    <StyledLoadingSpinner>
+    <StyledLoadingSpinner borderRadius={props.borderRadius || 0}>
       <LoadingCircle color={"var(--primary)"} size={40} />
     </StyledLoadingSpinner>
   );
