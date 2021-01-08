299b71bb032f12d3431e30d09fe63e76adbcd060
Chase Manning <me@chasemanning.co.nz>
2021-01-08 20:23:41 +1300

    :zap: always render analytics

--- apps/mobile/src/components/tabs/Analytics.tsx
+++ apps/mobile/src/components/tabs/Analytics.tsx
@@ -6,10 +6,12 @@ import { useSelector } from "react-redux";
 import { selectStats } from "../../state/dataSlice";
 import { selectStatsLoading } from "../../state/loadingSlice";
 import LoadingLine from "../shared/LoadingLine";
+import { selectActiveTab, Tab } from "../../state/navigationSlice";
+import { HideComponentProps } from "../../styles/Shared";
 
 const StyledAnalytics = styled.div`
+  display: ${(props: HideComponentProps) => (props.show ? "flex" : "none")};
   width: 100%;
-  display: flex;
   flex-direction: column;
   align-items: center;
   padding: 30px;
@@ -19,9 +21,10 @@ const StyledAnalytics = styled.div`
 const Analytics = () => {
   const stats = useSelector(selectStats);
   const analyticsLoading = useSelector(selectStatsLoading);
+  const activeTab = useSelector(selectActiveTab);
 
   return (
-    <StyledAnalytics>
+    <StyledAnalytics show={activeTab === Tab.Stats}>
       <LoadingLine loading={analyticsLoading} />
       {stats.map((stat: StatModel) => (
         <Stat key={stat.title} stat={stat} />
